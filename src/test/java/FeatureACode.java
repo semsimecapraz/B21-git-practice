9f03f9caf416e473bae8ac797b175af9aa4b3890
semsimecapraz <semsimecapraz@yahoo.com>
2021-01-26 14:23:44 -0500

    More work on featureA on new branch

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -3,5 +3,6 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work for A started");
+        System.out.println("B21 is learning more git with review");
     }
 }
