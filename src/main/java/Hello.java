612e16ca816c133c9efae5f6769518329a474237
semsimecapraz <semsimecapraz@yahoo.com>
2021-01-26 15:16:51 -0500

    movie 1 branch updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,8 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        System.out.println("B21 future SDET");
+        System.out.println("Hello World , B21");
+        System.out.println("B21 future SDET learnin Git");
 
     }
 
